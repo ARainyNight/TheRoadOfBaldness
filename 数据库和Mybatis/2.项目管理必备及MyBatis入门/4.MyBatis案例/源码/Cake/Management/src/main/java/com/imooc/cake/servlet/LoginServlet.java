9c7a8598e3a43567bea73cab7740adf8f6a33f77
ARainyNight <18234168426@163.com>
2018-10-26 13:25:00 +0800

    commit

---
+++ 数据库和Mybatis/2.项目管理必备及MyBatis入门/4.MyBatis案例/源码/Cake/Management/src/main/java/com/imooc/cake/servlet/LoginServlet.java
@@ -0,0 +1,32 @@
+package com.imooc.cake.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/***
+ *  ClassName : LoginServlet
+ *  Author    : lin
+ *  Date      : 2018/10/26 9:52    
+ *  Remark    : 登录
+ */
+
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if ("/login.do".equals(req.getServletPath())){
+            String username = req.getParameter("username");
+            String password = req.getParameter("password");
+            //如果用户名和密码相同且不为空，则登录成功
+            if (null!=username &&username.equals(password)){
+                req.getSession().setAttribute("username",username);
+                req.getRequestDispatcher("/cake/list.do").forward(req,resp);
+            }else {
+                req.getRequestDispatcher("/login.do").forward(req,resp);
+            }
+        }
+    }
+}
