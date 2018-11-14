63c1c26816d77f5aadb7beb4218a5c7ee95c7817
ARainyNight <18234168426@163.com>
2018-11-14 21:02:27 +0800

    commit

---
+++ SSM框架整合/1.Spring从入门到进阶/7.Spring+MyBatis整合开发案例/源码/sm1/sm_web/src/main/java/com/imooc/sm/controller/DepartmentController.java
@@ -0,0 +1,26 @@
+package com.imooc.sm.controller;
+
+import com.imooc.sm.entity.Department;
+import com.imooc.sm.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@Controller("departmentController")
+public class DepartmentController {
+    @Autowired
+    private DepartmentService departmentService;
+    //  /department/list.do     /department_list.jsp
+    public void list(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Department> list = departmentService.getAll();
+        request.setAttribute("LIST",list);
+        request.getRequestDispatcher("../department_list.jsp").forward(request,response);
+    }
+
+
+}
