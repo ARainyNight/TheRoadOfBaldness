dfddc95973f402c0abad8f398f09e04b6a4201b9
ARainyNight <18234168426@163.com>
2018-11-18 21:29:47 +0800

    commit

---
+++ SSM框架整合/2.MyBatis进阶/4.Spring+Mybatis整合案例之电商模块/源码/shopmgr/src/main/java/com/hn/shop/TestMain.java
@@ -0,0 +1,17 @@
+package com.hn.shop;
+
+import org.fkjava.dto.support.DTOHelper;
+
+/***
+ *  ClassName : TestMain
+ *  Author    : lin
+ *  Date      : 2018/11/18 21:16    
+ *  Remark    : 
+ */
+
+public class TestMain {
+    public static void main(String []args) throws Exception {
+        DTOHelper dto = new DTOHelper();
+        dto.createDto();
+    }
+}
---
+++ SSM框架整合/2.MyBatis进阶/4.Spring+Mybatis整合案例之电商模块/源码/shopmgr/src/main/java/com/hn/shop/repository/ArticleTypeMapper.java
@@ -0,0 +1,14 @@
+package com.hn.shop.repository;
+
+/**
+ * ArticleTypeMapper 数据访问类
+ * @author xlei @qq 251425887 @tel 13352818008
+ * @Email dlei0009@163.com
+ * @date 2018-11-18 21:17:31
+ * @version 1.0
+ */
+public interface ArticleTypeMapper {
+
+
+
+}
