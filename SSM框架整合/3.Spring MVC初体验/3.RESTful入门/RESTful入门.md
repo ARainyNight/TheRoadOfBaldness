b487604f4e44792e718371f97de51cd079d7d2a6
ARainyNight <18234168426@163.com>
2018-11-25 17:02:55 +0800

    commit

---
+++ SSM框架整合/3.Spring MVC初体验/3.RESTful入门/RESTful入门.md
@@ -0,0 +1,32 @@
+### 什么是RESTful
+
+REST:Representational State Transfer（表述性状态转移）
+
+REST并不是一种创新技术，它指的是一组架构约束条件和原则
+
+符合REST的约束条件和原则的架构，就称它为RESTful架构
+
+### RESTful核心内容
+
++ 资源与URI
+
++ 资源的表述
+
++ 状态转移
+
+### RESTful架构特点
+
+1. 统一了客户端访问资源的接口
+2. url更加简洁，易于理解，便于扩展
+3. 有利于不同系统之间的资源共享
+
+### RESTful风格概述
+
+RESTful具体来讲就是HTTP协议的四种形式表示四种基本操作
+
++ GET :获取资源
++ POST：新建资源
++ PUT：修改资源
++ DELETE：删除资源
+
+  
---
+++ SSM框架整合/3.Spring MVC初体验/3.RESTful入门/源码/SpringMVCRESTful/src/main/java/com/imooc/entity/Course.java
@@ -0,0 +1,38 @@
+package com.imooc.entity;
+
+/***
+ *  ClassName : Course
+ *  Author    : lin
+ *  Date      : 2018/11/24 12:40    
+ *  Remark    : 
+ */
+
+public class Course {
+    private int id ;
+    private String name ;
+    private double price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
