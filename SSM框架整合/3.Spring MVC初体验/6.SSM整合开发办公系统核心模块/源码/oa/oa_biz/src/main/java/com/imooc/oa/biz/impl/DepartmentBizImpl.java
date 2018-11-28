451256acd48651b224bda73b3d572a53011c6b4f
ARainyNight <18234168426@163.com>
2018-11-28 11:57:43 +0800

    commit

---
+++ SSM框架整合/3.Spring MVC初体验/6.SSM整合开发办公系统核心模块/源码/oa/oa_biz/src/main/java/com/imooc/oa/biz/impl/DepartmentBizImpl.java
@@ -0,0 +1,47 @@
+package com.imooc.oa.biz.impl;
+
+import com.imooc.oa.biz.DepartmentBiz;
+import com.imooc.oa.dao.DepartmentDao;
+import com.imooc.oa.entity.Department;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/***
+ *  ClassName : DepartmentBizImpl
+ *  Author    : lin
+ *  Date      : 2018/11/28 11:36    
+ *  Remark    : 
+ */
+
+@Service("departmentBiz")
+public class DepartmentBizImpl implements DepartmentBiz {
+
+    @Qualifier("departmentDao")
+    @Autowired
+    private DepartmentDao departmentDao ;
+
+
+    public void add(Department department) {
+        departmentDao.insert(department);
+    }
+
+    public void edit(Department department) {
+        departmentDao.update(department);
+    }
+
+    public void remove(String sn) {
+        departmentDao.delete(sn);
+    }
+
+    public Department get(String sn) {
+        return departmentDao.select(sn);
+    }
+
+    public List<Department> getAll() {
+        return departmentDao.selectAll();
+    }
+}
