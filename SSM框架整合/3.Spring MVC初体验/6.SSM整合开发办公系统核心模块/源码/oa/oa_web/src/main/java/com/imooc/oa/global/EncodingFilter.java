9e3f65c62e8a5227451540b959b745d356b4a6a1
ARainyNight <18234168426@163.com>
2018-12-01 20:35:03 +0800

    commit

---
+++ SSM框架整合/3.Spring MVC初体验/6.SSM整合开发办公系统核心模块/源码/oa/oa_web/src/main/java/com/imooc/oa/global/EncodingFilter.java
@@ -0,0 +1,39 @@
+package com.imooc.oa.global;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/***
+ *  ClassName : EncodingFilter
+ *  Author    : lin
+ *  Date      : 2018/12/1 20:00    
+ *  Remark    : 编码过滤器
+ */
+
+public class EncodingFilter implements Filter {
+
+    private String encoding = "utf-8";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        if (filterConfig.getInitParameter("encoding") != null) {
+            encoding = filterConfig.getInitParameter("encoding");
+        }
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        request.setCharacterEncoding(encoding);
+        response.setCharacterEncoding(encoding);
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
