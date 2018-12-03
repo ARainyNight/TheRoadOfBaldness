04fc409c73514ff13d40ffa23d933a0ed2227415
ARainyNight <18234168426@163.com>
2018-12-03 11:29:59 +0800

    commit

--- SSM框架整合/3.Spring MVC初体验/6.SSM整合开发办公系统核心模块/源码/oa/oa_web/src/main/java/com/imooc/oa/controller/DepartmentController.java
+++ SSM框架整合/3.Spring MVC初体验/6.SSM整合开发办公系统核心模块/源码/oa/oa_web/src/main/java/com/imooc/oa/controller/DepartmentController.java
@@ -1,6 +1,8 @@
 package com.imooc.oa.controller;
 
 import com.imooc.oa.biz.DepartmentBiz;
+import com.imooc.oa.entity.Department;
+import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,41 @@ public class DepartmentController {
     public String list(Map<String, Object> map) {
         map.put("list", departmentBiz.getAll());
         return "department_list";
+    }
+
+    /**
+     * 添加部门信息
+     * @param map
+     * @return
+     */
+    @RequestMapping("/to_add")
+    public String toAdd(Map<String, Object> map){
+        map.put("department",new Department());
+        return "department_add";
+    }
+
+    @RequestMapping("/add")
+    public String add(Department department){
+        departmentBiz.add(department);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value = "/to_update",params = "sn")
+    public String toUpdate(String sn, Map<String, Object> map){
+        map.put("department",departmentBiz.get(sn));
+        return "department_update";
+    }
+
+    @RequestMapping("/update")
+    public String update(Department department){
+        departmentBiz.edit(department);
+        return "redirect:list";
+    }
+
+    @RequestMapping(value = "/remove",params = "sn")
+    public String remove(String sn){
+        departmentBiz.remove(sn);
+        return "redirect:list";
     }
 
 }
