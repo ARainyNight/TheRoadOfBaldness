3307fe49c957d9b7638796535ff892c3cc30554b
ARainyNight <18234168426@163.com>
2018-12-03 16:50:37 +0800

    commit

---
+++ SSM框架整合/3.Spring MVC初体验/6.SSM整合开发办公系统核心模块/源码/oa/oa_biz/src/main/java/com/imooc/oa/biz/impl/EmployeeBizImpl.java
@@ -0,0 +1,46 @@
+package com.imooc.oa.biz.impl;
+
+import com.imooc.oa.biz.EmployeeBiz;
+import com.imooc.oa.dao.EmployeeDao;
+import com.imooc.oa.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/***
+ *  ClassName : EmployeeBizImpl
+ *  Author    : lin
+ *  Date      : 2018/12/3 16:08    
+ *  Remark    : 
+ */
+
+@Service("employeeBiz")
+public class EmployeeBizImpl implements EmployeeBiz {
+
+    @Qualifier("employeeDao")
+    @Autowired
+    private EmployeeDao employeeDao ;
+
+    public void add(Employee employee) {
+        employee.setPassword("000000");
+        employeeDao.insert(employee);
+    }
+
+    public void edit(Employee employee) {
+        employeeDao.update(employee);
+    }
+
+    public void remove(String sn) {
+        employeeDao.delete(sn);
+    }
+
+    public Employee get(String sn) {
+        return employeeDao.select(sn);
+    }
+
+    public List<Employee> getAll() {
+        return employeeDao.selectAll();
+    }
+}
