95a3ac4c0eb5bcb7e17e24bc10ccc063b854c235
ARainyNight <18234168426@163.com>
2018-12-04 21:42:04 +0800

    commit

--- SSM框架整合/3.Spring MVC初体验/6.SSM整合开发办公系统核心模块/SSM整合开发源代码/oa/oa_biz/src/main/java/com/imooc/oa/biz/GlobalBiz.java
+++
@@ -1,8 +0,0 @@
-package com.imooc.oa.biz;
-
-import com.imooc.oa.entity.Employee;
-
-public interface GlobalBiz {
-    Employee login(String sn,String password);
-    void changePassword(Employee employee);
-}
