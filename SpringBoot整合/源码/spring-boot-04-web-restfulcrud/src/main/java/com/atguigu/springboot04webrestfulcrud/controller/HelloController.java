5bfe708e4907b6b838d5629546f125a705924ed5
ARainyNight <18234168426@163.com>
2018-12-14 22:32:02 +0800

    commit

--- SpringBoot整合/源码/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot04webrestfulcrud/controller/HelloController.java
+++ SpringBoot整合/源码/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot04webrestfulcrud/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Arrays;
 import java.util.Map;
 
 /***
@@ -27,6 +28,7 @@ public class HelloController {
     @RequestMapping("/success")
     public String success(Map<String,Object> map){
         map.put("hello","小飞");
+        map.put("users", Arrays.asList("zhangsan","lisi","wangwu"));
         return "success";
     }
 }
