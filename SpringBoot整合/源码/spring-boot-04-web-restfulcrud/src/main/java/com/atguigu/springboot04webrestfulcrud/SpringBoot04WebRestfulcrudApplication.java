0d6bdeb7d2789bca2ecec862cb14a14d371a240f
ARainyNight <18234168426@163.com>
2018-12-15 11:45:16 +0800

    commit

--- SpringBoot整合/源码/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot04webrestfulcrud/SpringBoot04WebRestfulcrudApplication.java
+++ SpringBoot整合/源码/spring-boot-04-web-restfulcrud/src/main/java/com/atguigu/springboot04webrestfulcrud/SpringBoot04WebRestfulcrudApplication.java
@@ -2,11 +2,28 @@ package com.atguigu.springboot04webrestfulcrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class SpringBoot04WebRestfulcrudApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBoot04WebRestfulcrudApplication.class, args);
+    }
+
+    @Bean
+    public ViewResolver myViewResolver(){
+        return new MyViewResolver();
+    }
+
+    private static class MyViewResolver implements ViewResolver{
+        @Override
+        public View resolveViewName(String s, Locale locale) {
+            return null;
+        }
     }
 }
