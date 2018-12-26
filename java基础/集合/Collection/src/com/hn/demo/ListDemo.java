c3378ad0c1ebd9284ed1bd99ad05a0acbe3571cc
ARainyNight <18234168426@163.com>
2018-12-26 22:18:14 +0800

    commit

--- java基础/集合/Collection/src/com/hn/demo/ListDemo.java
+++ java基础/集合/Collection/src/com/hn/demo/ListDemo.java
@@ -2,6 +2,8 @@ package com.hn.demo;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 /***
  *  ClassName : ListDemo
@@ -12,6 +14,48 @@ import java.util.Iterator;
 
 public class ListDemo {
     public static void main(String []args){
+        //演示列表迭代器
+        ArrayList a2 = new ArrayList();
+        a2.add("java01");
+        a2.add("java02");
+        a2.add("java03");
+        a2.add("java04");
+        a2.add("java05");
+
+
+        ListIterator li = a2.listIterator();
+
+        System.out.println("hasNext():"+li.hasNext());  //true
+        System.out.println("hasPrevious():"+li.hasPrevious());  //false
+        while (li.hasNext()){
+            Object obj = li.next();
+            if (obj.equals("java02")){
+//                li.add("java009");  //[java01, java02, java009, java03, java04, java05]
+                li.set("java006"); //[java01, java006, java03, java04, java05]
+            }
+        }
+        System.out.println(a2);
+
+        System.out.println("hasNext():"+li.hasNext());   //false
+        System.out.println("hasPrevious():"+li.hasPrevious());   //true
+
+        /**
+         * 在迭代器过程中，准备添加或者删除元素
+         */
+//        Iterator it = a2.iterator();
+//        while (it.hasNext()){
+//            Object obj  = it.next() ;
+//            if (obj.equals("java02")){
+//                a2.add("java008");        //会照成同步错误
+//                it.remove();
+//            }
+//            System.out.println("obj="+obj);
+//        }
+//        System.out.println(a2);
+    }
+
+
+    public void method(){
         ArrayList al = new ArrayList();
 
         al.add("java01");
@@ -44,6 +88,11 @@ public class ListDemo {
             System.out.println("--"+it.next());
         }
 
+        //通过indexof获取位置
+        System.out.println(al.indexOf("java07"));
+
+        List sub = al.subList(1,3);
+        System.out.println(sub);
 
     }
 }
---
+++ java基础/集合/Collection/src/com/hn/demo/VectorDemo.java
@@ -0,0 +1,22 @@
+package com.hn.demo;
+
+import java.util.Vector;
+
+/***
+ *  ClassName : VectorDemo
+ *  Author    : lin
+ *  Date      : 2018/12/26 22:17    
+ *  Remark    : 
+ */
+
+public class VectorDemo {
+    public static void main(String []args){
+        Vector v =new Vector();
+
+        v.add("java01");
+        v.add("java02");
+        v.add("java03");
+        v.add("java04");
+        v.add("java05");
+    }
+}
